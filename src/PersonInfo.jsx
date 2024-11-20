0a8819fa90225df562bda40300ea5050b5075d97
anzhelika <anjiko333@gmail.com>
2024-11-20 18:39:45 +0600

    1

---
+++ src/PersonInfo.jsx
@@ -0,0 +1,18 @@
+// src/PersonInfo.jsx
+import React from "react";
+
+const PersonInfo = () => {
+  return (
+    <div>
+      <h1>Person Information</h1>
+      <p><strong>Name:</strong> Anzhelika Zhalynbekova</p>
+      <p><strong>Job Title:</strong> Software Engineer</p>
+      <p><strong>Company:</strong> NASA</p>
+      <p><strong>Location:</strong> Washington, USA</p>
+      <p><strong>Age:</strong> 28</p>
+      <p><strong>Hobbies:</strong> Traveling, Photography, Sky-diving</p>
+    </div>
+  );
+};
+
+export default PersonInfo;
